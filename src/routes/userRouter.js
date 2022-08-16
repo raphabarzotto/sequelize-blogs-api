56b3d13f302059ee23b10ed91c3c546fc9162b7d
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 13:31:00 -0300

    chore: add middleware to userRouter code

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,14 +5,14 @@ const middlewares = require('../middlewares');
 const userRouter = Router();
 
 // requisito 3, validatelogin + postLogin
-userRouter.post('/login', ValidateLogin, userControllers.postLogin)
+userRouter.post('/login', middlewares.validateLogin, userControllers.postLogin)
 // requisito 4, postUser
   .post('/user', userControllers.postUser)
 // requisito 5, validateToken + getAllUser
-  .get('/user', validateToken, userControllers.getAllUser)
+  .get('/user', middlewares.validateToken, userControllers.getAllUser)
 // requisito 6, validateToken + getByIdUser
-  .get('/user/:id', validateToken, userControllers.getByIdUser)
+  .get('/user/:id', middlewares.validateToken, userControllers.getByIdUser)
 // requisito 16, validateToken + deleteUser
-  .delete('/user/me', validateToken, userControllers.deleteUser);
+  .delete('/user/me', middlewares.validateToken, userControllers.deleteUser);
 
 module.exports = { userRouter };
