cedd93fcf315150ade86e16e3d513f816de2bee5
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 15:20:32 -0300

    feat: add router to api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
+const router = require('./routers')
 // ...
 
 const app = express();
 
 app.use(express.json());
-
+app.use(router);
 // ...
 
 // É importante exportar a constante `app`,
