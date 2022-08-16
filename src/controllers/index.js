ccdd11085d071b7316c382bb08bf8ff31586aa4d
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 16:10:53 -0300

    feat: add index to controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,9 @@
+const userControllers = require('./userControllers');
+const postControllers = require('./postControllers');
+const categoryControllers = require('./categoryControllers');
+
+module.exports = { 
+  userControllers,
+  postControllers,
+  categoryControllers,
+ };
