89a28dc4ce8bf6f1c5b32ded3293f790df460414
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 15:11:02 -0300

    fix: I accidentally mixed getbyId and putbyId in post Router - req 14 and 15

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -6,10 +6,12 @@ const postRouter = Router();
 
 // requisito 12, validateToken + postPost
 postRouter.post('/post', middlewares.validateToken, postControllers.postPost)
-// requisito 14, validateToken + getAllPost
+// requisito 13, validateToken + getAllPost
   .get('/post', middlewares.validateToken, postControllers.getAllPost)
-// requisito 15, validateToken + getByIdPost
-  .put('/post/:id', middlewares.validateToken, postControllers.getByIdPost)
+// requisito 14, validateToken + getByIdPost
+  .get('/post/:id', middlewares.validateToken, postControllers.getByIdPost)
+// requisito 15, validateToken + putByIdPost
+  .put('/post/:id', middlewares.validateToken, postControllers.putByIdPost)
 // requisito 16, validateToken + deletePost
   .delete('/post/:id', middlewares.validateToken, postControllers.deletePost)
 // requisito 18, validateToken + getBySearchPost
