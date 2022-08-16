8bc26744baacc9aeacc6debd0930e563bead8e84
raphabarzotto <raphabarzotto@gmail.com>
2022-08-15 21:54:47 -0300

    feat: add categories migration

---
+++ src/database/migrations/20220816004943-createCategories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        timestamps: false,
+      }
+    );
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Categories');
+  },
+};
