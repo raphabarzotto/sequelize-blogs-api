b2dc609481bd69bc722aad025dcc820d7a178599
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 18:16:44 -0300

    feat: add handleError to api.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
-// ...
+const { handleError } = require('./middlewares');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+// antes ou depois?
+app.use(errorMiddleware);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
