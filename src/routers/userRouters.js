d64508c6b75ac10591271f325a5dc958cd5c5650
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 23:34:51 -0300

    feat: add to userRouters - getAllUser

--- src/routers/userRouters.js
+++ src/routers/userRouters.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const { userControllers } = require('../controllers');
+const { handleToken } = require('../middlewares');
 
 const userRouters = express.Router();
 
 userRouters.post('/', rescue(userControllers.postUser));
+userRouters.get('/', handleToken, rescue(userControllers.getAllUser));
 
 module.exports = userRouters;
