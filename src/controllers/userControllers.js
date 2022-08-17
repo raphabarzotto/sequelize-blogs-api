301ddcdfdd2e78c8cea2d651c85f615defbe266c
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 22:08:18 -0300

    feat: add to userController - postUser

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,32 @@
+const { User } = require('../database/models');
+const validateUserJoi = require('../helpers');
+const { tokenGenerator } = require('../helpers');
+
+// requisito 4, código muito parecido com o que fiz no Store Manager
+const postUser = async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+
+  const { error } = validateUserJoi.validate(req.body);
+
+  if (error) {
+    const [code, message] = error.message.split('|');
+    return res.status(code).json({ message });
+  }
+
+  // https://sequelize.org/docs/v6/core-concepts/model-querying-finders/
+  const [user, created] = await User.findOrCreate({
+    where: { displayName, email, password, image },  
+  });
+
+  if (!created) {
+    return res.status(409).json({ message: 'User already registered' });
+  }
+
+  const token = tokenGenerator({ email: user.dataValues.email });
+
+  return res.status(201).json({ token });
+};
+
+module.exports = {
+  postUser
+}
