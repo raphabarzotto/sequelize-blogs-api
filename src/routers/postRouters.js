07d20ea3877f33fb17617a567779ae9aaddf3745
raphabarzotto <raphabarzotto@gmail.com>
2022-08-17 02:19:05 -0300

    feat: add to postRouters - getByIdBlogPost

--- src/routers/postRouters.js
+++ src/routers/postRouters.js
@@ -7,5 +7,6 @@ const postRoute = express.Router();
 
 postRoute.post('/', handleToken, rescue(postControllers.postBlogPost));
 postRoute.get('/', handleToken, rescue(postControllers.getAllBlogPost));
+postRoute.get('/:id', handleToken, rescue(postControllers.getByIdBlogPost));
 
 module.exports = postRoute;
