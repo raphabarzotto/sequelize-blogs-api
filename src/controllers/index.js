b2ec0369e37c802b5586c7fb8be234298ce8a42c
raphabarzotto <raphabarzotto@gmail.com>
2022-08-17 01:52:40 -0300

    feat: add postControllers to index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const loginController = require('./loginControllers');
 const userControllers = require('./userControllers');
 const categoryControllers = require('./categoryControllers');
+const postControllers = require('./postControllers');
 
 module.exports = { 
   loginController,
   userControllers,
   categoryControllers,
+  postControllers,
 };
