d9f596e57c4ee06b7a17e86ca925ee688767c69c
raphabarzotto <raphabarzotto@gmail.com>
2022-08-17 01:55:12 -0300

    feat: add postRouters to index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 const loginRouter = require('./loginRouters');
 const userRouters = require('./userRouters');
 const categoryRouters = require('./categoryRouters');
+const postRouters = require('./postRouters');
 
 module.exports = { 
   loginRouter,
   userRouters,
   categoryRouters,
+  postRouters,
 };
