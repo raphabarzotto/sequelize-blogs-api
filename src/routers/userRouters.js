11a5758899dc869e569418f6491037f36efcf27b
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 23:56:02 -0300

    feat: add to userRouters - getByIdUser

--- src/routers/userRouters.js
+++ src/routers/userRouters.js
@@ -7,5 +7,6 @@ const userRouters = express.Router();
 
 userRouters.post('/', rescue(userControllers.postUser));
 userRouters.get('/', handleToken, rescue(userControllers.getAllUser));
+userRouters.get('/:id', handleToken, rescue(userControllers.getByIdUser));
 
 module.exports = userRouters;
