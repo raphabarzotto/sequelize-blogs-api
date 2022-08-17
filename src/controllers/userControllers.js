923da717cd1aaa53eb6fdd642a448b5cb0542afa
raphabarzotto <raphabarzotto@gmail.com>
2022-08-16 23:31:41 -0300

    feat: add to userController - getAllUser

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,6 +2,8 @@ const { User } = require('../database/models');
 const { validateUserJoi, tokenGenerator } = require('../helpers');
 
 // requisito 4, código muito parecido com o que fiz no Store Manager
+// https://sequelize.org/docs/v6/core-concepts/model-querying-finders/
+// usei a documentação para procurar a melhor query
 const postUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
 
@@ -12,7 +14,6 @@ const postUser = async (req, res) => {
     return res.status(code).json({ message });
   }
 
-  // https://sequelize.org/docs/v6/core-concepts/model-querying-finders/
   const [user1, created] = await User.findOrCreate({
     where: { displayName, email, password, image },  
   });
@@ -26,6 +27,13 @@ const postUser = async (req, res) => {
   return res.status(201).json({ token });
 };
 
+const getAllUser = async (_req, res) => {
+  const users = await User.findAll({ attributes: { exclude: ['password'] } });
+  
+  return res.status(200).json(users);
+};
+
 module.exports = {
   postUser,
+  getAllUser,
 };
